a463038f6edcdc44e6064d35fa3302aac2c8665c
frank <franklin.neves.filho@gmail.com>
2024-08-06 09:05:24 -0400

    Refactored Response Factory

--- utils/factories/response.py
+++ utils/factories/response.py
@@ -6,12 +6,11 @@ This model is used to create a response object that can be returned by the servi
 
 
 class Response:
-    def __init__(self, node=None, errors=None, status=200):
+    def __init__(self, node=None, errors=None):
         if errors is None:
             errors = []
         self.node = node
         self.errors = errors
-        self.status = status
 
     def get_errors(self):
         return self.errors
@@ -19,15 +18,12 @@ class Response:
     def get_node(self):
         return self.node
 
-    def get_status(self):
-        return self.status
-
-    def to_dict(self):
+    def dict(self):
         return {
             'node': self.node,
             'errors': self.errors,
-            'status': self.status
         }
 
     def __repr__(self):
-        return str(self.to_dict())
+        return str(self.dict())
+
--- utils/factories/response_factory.py
+++ utils/factories/response_factory.py
@@ -1,8 +1,10 @@
 from .response import Response
+from fastapi.responses import JSONResponse
 
 status_codes = {
     'success': 200,
     'created': 201,
+    'no_content': 204,
     'bad_request': 400,
     'unauthorized': 401,
     'forbidden': 403,
@@ -15,53 +17,59 @@ status_codes = {
 class ResponseFactory:
 
     @staticmethod
-    def _generate_response(node=None, errors=None, status='success'):
-        if errors is None:
-            errors = []
-        return Response(node=node, errors=errors, status=status_codes[status])
+    def _generate_response(status: str, response: Response = None):
+        return JSONResponse(
+            content=response.dict(),
+            status_code=status_codes[status]
+        )
 
     @staticmethod
-    def generate_error_response(message=None):
-        return ResponseFactory._generate_response(errors=message, status='bad_request')
+    def generate_error_response(response: Response):
+        return ResponseFactory._generate_response(response=response, status='bad_request')
 
     @staticmethod
-    def generate_ok_response(node=None):
-        return ResponseFactory._generate_response(node=node, status='success')
+    def generate_ok_response(response: Response):
+        return ResponseFactory._generate_response(response=response, status='success')
 
     @staticmethod
-    def generate_created_response(node=None):
-        return ResponseFactory._generate_response(node=node, status='created')
+    def generate_created_response():
+        return ResponseFactory._generate_response(status='created')
 
     @staticmethod
-    def generate_bad_request_response(errors=None):
-        return ResponseFactory._generate_response(errors=errors, status='bad_request')
+    def generate_bad_request_response(response: Response):
+        return ResponseFactory._generate_response(response=response, status='bad_request')
 
     @staticmethod
-    def generate_unauthorized_response(errors=None):
-        return ResponseFactory._generate_response(errors=errors, status='unauthorized')
+    def generate_unauthorized_response():
+        return ResponseFactory._generate_response(status='unauthorized')
 
     @staticmethod
-    def generate_forbidden_response(errors=None):
-        return ResponseFactory._generate_response(errors=errors, status='forbidden')
+    def generate_forbidden_response():
+        return ResponseFactory._generate_response(status='forbidden')
 
     @staticmethod
-    def generate_not_found_response(errors=None):
-        return ResponseFactory._generate_response(errors=errors, status='not_found')
+    def generate_not_found_response(response: Response):
+        return ResponseFactory._generate_response(response=response, status='not_found')
 
     @staticmethod
-    def generate_conflict_response(errors=None):
-        return ResponseFactory._generate_response(errors=errors, status='conflict')
+    def generate_conflict_response(response: Response):
+        return ResponseFactory._generate_response(response=response, status='conflict')
 
     @staticmethod
-    def generate_internal_server_error_response(errors=None):
-        return ResponseFactory._generate_response(errors=errors, status='internal_server_error')
+    def generate_internal_server_error_response(response: Response):
+        return ResponseFactory._generate_response(response=response, status='internal_server_error')
 
     @staticmethod
-    def generate_response(node=None, errors=None, status='success'):
-        return ResponseFactory._generate_response(node=node, errors=errors, status=status)
+    def generate_response(response: Response, status='success'):
+        return ResponseFactory._generate_response(response=response, status=status)
 
     @staticmethod
     def generate_response_from_exception(exception):
-        return ResponseFactory._generate_response(errors=[str(exception)], status='internal_server_error')
+        return ResponseFactory._generate_response(
+            response=Response(errors=[str(exception)]), status='internal_server_error')
+
+    @staticmethod
+    def generate_no_content_response():
+        return ResponseFactory._generate_response(status='no_content')
 
 
